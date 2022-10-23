c754a945c2bc5946639afa01d65dcf5230d61ade
alimoqimi97 <alimoqimi97@gmail.com>
2022-10-23 11:16:31 +0330

    feature#1: jit mode added
    tailwindcss jit mode added to project.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: 'jit',
+  purge: [
+    './**/*.html',
+    './src/**/*.{js,jsx,ts,tsx,vue}',
+  ],
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
