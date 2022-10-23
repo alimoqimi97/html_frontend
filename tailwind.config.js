f254126d47d413659c80bec25122b63454a24de6
alimoqimi97 <alimoqimi97@gmail.com>
2022-10-23 14:37:45 +0330

    responsiveness config
    responsiveness config set.

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,19 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {},
+    screens: {
+      'sm': '480px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '744px',
+      // => @media (min-width: 640px) { ... }
+
+      'lg': '980px',
+      // => @media (min-width: 640px) { ... }
+
+      'xl': '1442px',
+      // => @media (min-width: 640px) { ... }
+    },
   },
   plugins: [],
 }
