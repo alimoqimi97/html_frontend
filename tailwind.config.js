613181bff35e6c10959cc77559079b23dffae848
adibNo9 <adib.mokhtari@gmail.com>
2022-10-25 10:28:20 +0330

    class page is prepared...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  mode: 'jit',
-  purge: [
-    './**/*.html',
-    './src/**/*.{js,jsx,ts,tsx,vue}',
-  ],
-  content: ["./**/*.{html,js}"],
+  content: ["./pages/**/*.{html,js}"],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: "0.6rem",
+      sm: "0.8rem",
+      base: "1rem",
+      xl: "1.25rem",
+    },
+    extend: {
+      height: {
+        264: "66rem",
+      },
+      width: {
+        22: "5.5rem",
+        34: "8.5rem",
+      },
+      translate: {
+        0.75: "0.1875rem",
+      },
+    },
+    screens: {
+      xs: "360px",
+      sm: "480px",
+      md: "744px",
+      lg: "980px",
+      xl: "1440px",
+    },
   },
-  plugins: [],
-}
+  plugins: [require("tailwindcss-dir")()],
+};
